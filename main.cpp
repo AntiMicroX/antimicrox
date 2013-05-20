34bc39d65cbeaace492a3958e316e87713a8cdd5
Travis Nickles <nickles.travis@gmail.com>
2013-05-19 20:45:02 -0500

    Re-added hook for saving the list of most recently opened configuration files when quitting the application

--- main.cpp
+++ main.cpp
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
     QObject::connect(&w, SIGNAL(joystickRefreshRequested()), joypad_worker, SLOT(refresh()));
     QObject::connect(joypad_worker, SIGNAL(joystickRefreshed(Joystick*)), &w, SLOT(fillButtons(Joystick*)));
     QObject::connect(&w, SIGNAL(joystickRefreshRequested(Joystick*)), joypad_worker, SLOT(refreshJoystick(Joystick*)));
-    //QObject::connect(&a, SIGNAL(aboutToQuit()), &w, SLOT(saveAppConfig()));
+    QObject::connect(&a, SIGNAL(aboutToQuit()), &w, SLOT(saveAppConfig()));
     QObject::connect(&a, SIGNAL(aboutToQuit()), joypad_worker, SLOT(quit()));
 
     w.show();
