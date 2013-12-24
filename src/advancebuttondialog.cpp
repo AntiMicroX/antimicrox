c6a4b2e01484ca281d459ec5832e11e1234b14c2
Travis Nickles <nickles.travis@gmail.com>
2013-12-24 17:18:48 -0600

    Workaround for Unity. AdvanceButtonDialog should display as intended. Resolves issue #40.
    The disadvantage of this commit is that the behavior in Gnome
    has been slightly altered. Window cannot be maximized nor minimized
    but it isn't as big of a deal with this window.

--- src/advancebuttondialog.cpp
+++ src/advancebuttondialog.cpp
@@ -12,7 +12,7 @@
 const int AdvanceButtonDialog::MINIMUMTURBO = 2;
 
 AdvanceButtonDialog::AdvanceButtonDialog(JoyButton *button, QWidget *parent) :
-    QDialog(parent, Qt::Window),
+    QDialog(parent, Qt::Dialog),
     ui(new Ui::AdvanceButtonDialog)
 {
     ui->setupUi(this);
