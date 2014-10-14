e806f2189bef5dee8bc1d1a03a7c5a5d9888760e
Travis Nickles <nickles.travis@gmail.com>
2014-10-14 05:27:10 -0500

    Updated to version 2.7.

--- src/commandlineutility.cpp
+++ src/commandlineutility.cpp
@@ -1,4 +1,4 @@
-#include <QDebug>
+//#include <QDebug>
 #include <QStringListIterator>
 #include <QFileInfo>
 #include <QTextStream>
--- src/joyaxis.cpp
+++ src/joyaxis.cpp
@@ -1,4 +1,4 @@
-#include <QDebug>
+//#include <QDebug>
 #include <cmath>
 
 #include "joyaxis.h"
