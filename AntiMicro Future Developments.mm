423245ba377765102ebc16ae91589d24cf013c07
Travis Nickles <nickles.travis@gmail.com>
2014-10-16 08:53:42 -0500

    Updated mind map.

--- AntiMicro Future Developments.mm
+++ AntiMicro Future Developments.mm
@@ -7,25 +7,29 @@
 <node CREATED="1379742754449" HGAP="21" ID="ID_146831130" MODIFIED="1410323646524" POSITION="right" TEXT="Copy Set Assignments to New Set" VSHIFT="-7">
 <icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1398676376577" FOLDED="true" HGAP="19" ID="ID_1223827762" MODIFIED="1399599674842" POSITION="left" TEXT="Split Actions into a Seperate Entity?" VSHIFT="-5">
+<node CREATED="1398676376577" FOLDED="true" HGAP="19" ID="ID_1223827762" MODIFIED="1413467597336" POSITION="left" TEXT="Split Actions into a Seperate Entity?" VSHIFT="-5">
 <icon BUILTIN="help"/>
 <node CREATED="1398676422304" ID="ID_744925725" MODIFIED="1398676855592" TEXT="Unlike the current system where assignment slots are assigned to controller buttons, actions would hold all applicable slots and a button would then be assigned an action. The major benefit behind doing that would be that complex macro assignments can be assigned to multiple buttons without completely mapping the sequence out again. Also, it would be much easier to assign the sequence to a different button.&#xa;&#xa;This type of change would probably require a complete program refactor. I honestly don&apos;t see this feature ever being incorporated into AntiMicro."/>
 </node>
-<node CREATED="1398676874185" FOLDED="true" HGAP="27" ID="ID_282049175" MODIFIED="1399029272306" POSITION="right" TEXT="Improve SteamOS Support" VSHIFT="-3">
+<node CREATED="1398676874185" FOLDED="true" HGAP="27" ID="ID_282049175" MODIFIED="1413467532586" POSITION="right" TEXT="Improve SteamOS Support" VSHIFT="-3">
 <icon BUILTIN="button_ok"/>
 <node CREATED="1398676892603" ID="ID_226300967" MODIFIED="1398677002203" TEXT="Auto Profile support should work in the SteamOS BPM session. Allow for more control over the process via the command line."/>
 </node>
-<node CREATED="1398677022243" HGAP="22" ID="ID_1460866844" MODIFIED="1398677343259" POSITION="right" TEXT="Refactor Main Interface for Different Devices" VSHIFT="-6"/>
+<node CREATED="1398677022243" HGAP="22" ID="ID_1460866844" MODIFIED="1413467519686" POSITION="right" TEXT="Refactor Main Interface for Different Devices" VSHIFT="-6">
+<icon BUILTIN="button_ok"/>
+</node>
 <node CREATED="1398677269018" HGAP="14" ID="ID_1391547769" MODIFIED="1398677391581" POSITION="left" TEXT="Wizards for Assigning More Complex Actions" VSHIFT="-7"/>
-<node CREATED="1398677595039" ID="ID_356404126" MODIFIED="1398677644585" POSITION="right" TEXT="Add Documentation for Portions of the Program"/>
-<node CREATED="1398682394740" FOLDED="true" ID="ID_1304074991" MODIFIED="1399029252404" POSITION="left" TEXT="Delay Slot Type">
+<node CREATED="1398677595039" ID="ID_356404126" MODIFIED="1413467504419" POSITION="right" TEXT="Add Documentation for Portions of the Program">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1398682394740" FOLDED="true" ID="ID_1304074991" MODIFIED="1413467543985" POSITION="left" TEXT="Delay Slot Type">
 <icon BUILTIN="button_ok"/>
 <node CREATED="1398682402644" ID="ID_693533160" MODIFIED="1398682514273" TEXT="A Delay slot will work almost like the current Hold slot type. The big difference is that any currently active slots will not be released after the delay time has been exceeded."/>
 </node>
 <node CREATED="1399110391534" ID="ID_198612384" MODIFIED="1400563261415" POSITION="right" TEXT="Convert to using CMake instead of qmake">
 <icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1399164906083" FOLDED="true" ID="ID_719184118" MODIFIED="1399599636302" POSITION="left" TEXT="Add Profile Names">
+<node CREATED="1399164906083" FOLDED="true" ID="ID_719184118" MODIFIED="1413467569556" POSITION="left" TEXT="Add Profile Names">
 <icon BUILTIN="button_ok"/>
 <node CREATED="1399164933013" ID="ID_1336467156" MODIFIED="1399165195098" TEXT="Currently, the basename of the profile filename is displayed in the interface to identify the profile. The filebrowser application bundled with Mezzanine alters filenames of files uploaded to the server. Filenames are converted to lowercase and spaces are converted to underscores. Because of this, the intended profile display name will not show up for someone who downloads the profile.&#xa;&#xa;There are other cases where it would be beneficial for the displayed name of the profile to be different from the associated profile filename."/>
 </node>
