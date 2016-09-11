eba3d5bba0a68ef024f92bd7200007da213ddad0
Jeff Backus <jeff@jsbackus.com>
2016-09-10 22:01:45 -0400

    Expanded debug logging of current window to display all relevant information.

--- src/autoprofilewatcher.cpp
+++ src/autoprofilewatcher.cpp
@@ -128,8 +128,9 @@ void AutoProfileWatcher::runAppCheck()
         currentAppWindowTitle = nowWindowName;
         //currentApplication = appLocation;
 
-	Logger::LogDebug(QObject::tr("Active window now \"%1\" (\"%2\").").
-			 arg(currentAppWindowTitle, currentApplication));
+	Logger::LogDebug(QObject::tr("Active window changed to: Title = \"%1\", "
+				     "Class = \"%2\", Program = \"%3\" or \"%4\".").
+			 arg(nowWindowName, nowWindowClass, appLocation, baseAppFileName));
 
         QSet<AutoProfileInfo*> fullSet;
 
