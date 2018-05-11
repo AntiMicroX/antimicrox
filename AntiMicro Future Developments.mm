5b9250167407bfd80bc22ad77b4e811512872dab
juliagoda <juliagoda.de@gmail.com>
2018-05-11 20:38:14 +0200

    Small improvements in Antimicro Future Developments file

--- AntiMicro Future Developments.mm
+++ AntiMicro Future Developments.mm
@@ -1,7 +1,9 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1379712845116" ID="ID_571348173" MODIFIED="1420094589396" TEXT="AntiMicro Future Developments">
-<node CREATED="1526058011028" ID="ID_121032400" MODIFIED="1526058221285" POSITION="right" TEXT="Add controller calibration">
+<node CREATED="1379712845116" ID="ID_571348173" MODIFIED="1526063409822" TEXT="Antimicro Future Developments">
+<font BOLD="true" NAME="Arial Black" SIZE="18"/>
+<node CREATED="1526058011028" ID="ID_121032400" MODIFIED="1526063318879" POSITION="right" TEXT="Add controller calibration">
+<font BOLD="true" NAME="Verdana" SIZE="14"/>
 <icon BUILTIN="idea"/>
 <icon BUILTIN="full-1"/>
 <node CREATED="1526058584673" ID="ID_380767629" MODIFIED="1526058599436" TEXT="&quot;We should be able to calibrate it by moving it to its extremes after clicking a button so that the software knows the maximum values for both axes, then click &quot;Finish calibrating&quot; when the joystick is at the center so it knows where that is too.&quot;">
@@ -9,7 +11,8 @@
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
-<node CREATED="1526058231348" ID="ID_912857029" MODIFIED="1526060209043" POSITION="left" TEXT="Create multiple profiles working with Auto Profiles ">
+<node CREATED="1526058231348" ID="ID_912857029" MODIFIED="1526063334597" POSITION="left" TEXT="Create multiple profiles working with Auto Profiles ">
+<font BOLD="true" NAME="Verdana" SIZE="14"/>
 <icon BUILTIN="idea"/>
 <icon BUILTIN="full-2"/>
 <node CREATED="1526058437842" ID="ID_49023724" MODIFIED="1526058549973" TEXT="&quot;You have your game profiles that are designed for certain games and will run only when certain games are running. You will have one profile called Media Player which is able to control your media player with your controller. This Media Player profile will always be running no matter what. You will have a mic on/off profile which is able to turn your mic on and off so you can talk by pressing a button with your controller. This profile will only run with selected games you choose it to run with. &#xa;&#xa;With this feature game profiles can be more dynamic since you don&apos;t need to add your media player buttons into every game profile, or if you want to change your mic button instead of editing every game profile you only need to edit one profile.&quot;">
@@ -17,7 +20,8 @@
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
-<node CREATED="1526058647554" ID="ID_167899793" MODIFIED="1526060450917" POSITION="right" TEXT="Include an option to integrate xwiimote">
+<node CREATED="1526058647554" ID="ID_167899793" MODIFIED="1526063321862" POSITION="right" TEXT="Include an option to integrate xwiimote">
+<font BOLD="true" NAME="Verdana" SIZE="14"/>
 <icon BUILTIN="full-5"/>
 <icon BUILTIN="idea"/>
 <node CREATED="1526058855400" ID="ID_1241861703" MODIFIED="1526058864274" TEXT="&quot;It is made for developers to add wii controller options to their games; virtually no consideration to users who just want to use their wiimote as a game controller for existing games. Actually, an xwiimote developer has suggested an even narrower scope, adding wiimotes to emulators. By default, 7 buttons are mapped to a game controller. no dpad, no nunchuck, no classic, no balance board, no guitar, no drums, no tilt, no accelerometer for game controllers. xwiimote does include these, by default, they are part of a keyboard device&quot;">
@@ -28,7 +32,8 @@
 </node>
 </node>
 </node>
-<node CREATED="1526059143094" ID="ID_1649392587" MODIFIED="1526060474456" POSITION="left" TEXT="Add improvements for flight simulator joystick">
+<node CREATED="1526059143094" ID="ID_1649392587" MODIFIED="1526063306065" POSITION="left" TEXT="Add improvements for flight simulator joystick">
+<font BOLD="true" NAME="Verdana" SIZE="14"/>
 <icon BUILTIN="full-6"/>
 <icon BUILTIN="idea"/>
 <node CREATED="1526059253123" ID="ID_1822846998" MODIFIED="1526059262354" TEXT="&quot;With a fast enough rate it would be possible to change the game&apos;s movement speed based on the joystick&apos;s analog value. Something like an &quot;enable duty cycle&quot; option&quot;">
@@ -36,23 +41,27 @@
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
-<node CREATED="1526059389070" ID="ID_129988638" MODIFIED="1526060498110" POSITION="right" TEXT="Add support for Naga mouse">
+<node CREATED="1526059389070" ID="ID_129988638" MODIFIED="1526063324630" POSITION="right" TEXT="Add support for Naga mouse">
+<font BOLD="true" NAME="Verdana" SIZE="14"/>
 <icon BUILTIN="full-7"/>
 <icon BUILTIN="idea"/>
 </node>
-<node CREATED="1526059514353" ID="ID_9232762" MODIFIED="1526060518431" POSITION="left" TEXT="Add support for Elite controller&apos;s paddles">
+<node CREATED="1526059514353" ID="ID_9232762" MODIFIED="1526063309806" POSITION="left" TEXT="Add support for Elite controller&apos;s paddles">
+<font BOLD="true" NAME="Verdana" SIZE="14"/>
 <icon BUILTIN="full-8"/>
 <icon BUILTIN="idea"/>
 </node>
-<node CREATED="1526059782198" ID="ID_1400167093" MODIFIED="1526060432725" POSITION="right" TEXT="Detecting window title part for loading profiles">
+<node CREATED="1526059782198" ID="ID_1400167093" MODIFIED="1526063382016" POSITION="right" STYLE="fork" TEXT="Detecting window title part for loading profiles">
+<font BOLD="true" NAME="Verdana" SIZE="14"/>
 <icon BUILTIN="full-4"/>
 <icon BUILTIN="yes"/>
-<node CREATED="1526060105290" ID="ID_1284273032" MODIFIED="1526060187449" TEXT="Why: Full text of window title can be different every time, when it&apos;s started">
+<node CREATED="1526060105290" ID="ID_1284273032" MODIFIED="1526063382016" TEXT="Why: Full text of window title can be different every time, when it&apos;s started">
 <cloud/>
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
-<node CREATED="1526060283067" ID="ID_1015249776" MODIFIED="1526060411917" POSITION="left" TEXT="Add compatibility for profiles from JoyToKey, Xpadder and Pinnacle Gamer Profiler">
+<node CREATED="1526060283067" ID="ID_1015249776" MODIFIED="1526063313615" POSITION="left" TEXT="Add compatibility for profiles from JoyToKey, Xpadder and Pinnacle Gamer Profiler">
+<font BOLD="true" NAME="Verdana" SIZE="14"/>
 <icon BUILTIN="idea"/>
 <icon BUILTIN="full-3"/>
 </node>
