1a0e84becd2cffa684dbf565364be03fb63a9bac
juliagoda <juliagoda.de@gmail.com>
2018-12-10 22:27:46 +0100

    Removed unused files

--- src/joybuttontypes/xml/joyaxisbuttonxml.cpp
+++
@@ -1,7 +0,0 @@
-#include "joyaxisbuttonxml.h"
-#include "joybuttontypes/joyaxisbutton.h"
-
-JoyAxisButtonXml::JoyAxisButtonXml(JoyAxisButton* joyAxisBtn, QObject* parent) : JoyButtonXml(joyAxisBtn, parent)
-{
-    m_joyAxisBtn = joyAxisBtn;
-}
--- src/xml/vdpadxml.cpp
+++
@@ -1,7 +0,0 @@
-#include "vdpadxml.h"
-#include "vdpad.h"
-
-VDPadXml::VDPadXml(VDPad* joyvdpad, QObject *parent) : JoyDPadXml(joyvdpad, parent)
-{
-    m_vdpad = joyvdpad;
-}
