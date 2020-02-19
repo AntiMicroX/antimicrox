0e23ac8c9034e2f1518167b3625fa0a4dee6e0c4
juliagoda <juliagoda.pl@protonmail.com>
2020-02-19 22:38:44 +0100

    Preparation for GUI tests - part I

--- tests/testaddeditautoprofiledialog.cpp
+++ tests/testaddeditautoprofiledialog.cpp
@@ -3,12 +3,69 @@
 #include <QtTest/QtTest>
 
 
+/*
+
+    AutoProfileInfo* getAutoProfile() const; // if not empty then
+    1) profileLineEdit must be equal to getAutoProfile()->getProfileLocation()
+    2) devicesComboBox must be equal to getAutoProfile()->getDeviceName()
+    3) winClassLineEdit must be equal to getAutoProfile()->getWindowClass()
+    4) if getAutoProfile()->isPartialState() then should be setPartialCheckBox->checked()
+    5) if getAutoProfile()->isCurrentDefault() then should be asDefaultCheckBox->checked()
+    6) if getAutoProfile()->getWindowName() is not empty then should be winNameLineEdit->text() == getAutoProfile()->getWindowName()
+    7) if getAutoProfile()->getExe() is not empty then should be applicationLineEdit->text() == getAutoProfile()->getExe()
+
+    //QString getOriginalGUID() const;
+    QString getOriginalUniqueID() const;
+    QString getOriginalExe() const;
+    QString getOriginalWindowClass() const;
+    QString getOriginalWindowName() const;
+
+    QList<InputDevice*> *getDevices() const; // cannot be empty
+    AntiMicroSettings *getSettings() const; // cannot be empty
+    bool getEditForm() const;
+    bool getDefaultInfo() const;
+    // QList<QString> const& getReservedGUIDs();
+    QList<QString> const& getReservedUniques(); // cannot be empty
+
+
+    profileBrowsePushButton not disabled
+    detectWinPropsSelectWindowPushButton not disabled
+    devicesComboBox not disabled and not empty
+    asDefaultCheckBox not disabled if devicesComboBox first element is not -1
+    asDefaultCheckBox disabled if devicesComboBox first element is -1
+    buttonBox Cancel not disabled
+    applicationPushButton not disabled
+    selectWindowPushButton not disabled
+    profileLineEdit not disabled
+    winClassLineEdit not disabled
+    winNameLineEdit not disabled
+    applicationLineEdit not disabled
+    buttonBox Ok disabled if profileLineEdit is empty and doesn't exist and devicesComboBox first element is empty and winClassLineEdit is empty
+    buttonBox Ok enabled if profileLineEdit isn't empty and exist and devicesComboBox first element is not empty and winClassLineEdit is not empty
+
+    test keyClick on profileBrowsePushButton
+    test keyClick on detectWinPropsSelectWindowPushButton
+    test keyClick on buttonBox->cancel
+    test keyClick on applicationPushButton
+    test keyClick on selectWindowPushButton
+
+    test mouseClick on profileBrowsePushButton
+    test mouseClick on detectWinPropsSelectWindowPushButton
+    test mouseClick on buttonBox->cancel
+    test mouseClick on applicationPushButton
+    test mouseClick on selectWindowPushButton
+*/
+
+
 class TestAddEditAutoProfileDialog: public QObject
 {
     Q_OBJECT
 
 public:
     TestAddEditAutoProfileDialog(QObject* parent = 0);
+    void checkAutoProfile();
+    void checkOriginalUniqueIDs();
+
 
 private slots:
 
--- tests/testbuttoneditdialog.cpp
+++ tests/testbuttoneditdialog.cpp
@@ -2,6 +2,34 @@
 
 #include <QtTest/QtTest>
 
+/*
+if slotSummaryLabel->text() == tr("[NO KEY]") and lastJoyButton is nullptr lastJoyButton->getSlotsString().replace("&", "&&") == tr("[NO KEY]") then advancedPushButton should be disabled
+
+if slotSummaryLabel->text() != tr("[NO KEY]") and lastJoyButton is not nullptr and lastJoyButton->getSlotsString().replace("&", "&&") != tr("[NO KEY]") then
+advancedPushButton should be enabled
+
+toggleCheckBox should be checked if lastJoyButton->getToggleState()
+
+turboCheckBox should be checked if lastJoyButton->isUsingTurbo()
+
+buttonNameLineEdit cannot be disabled
+
+actionNameLineEdit cannot be disabled
+
+buttonBox->Cancel cannot be disabled
+
+virtualKeyMouseTabWidget->tab(0)->->findChildren<VirtualKeyPushButton*> ()
+check if each
+QString getXcodestring() const; != ""
+QString getDisplayString() const; != tr("[NO KEY]")
+int getKeycode() const; != 0
+int getQkeyalias() const; != 0
+only if it's not a none button and otherKeysPushbutton
+
+getOtherKeysMenu()-actions()
+if AntKeyMapper::getInstance()->returnQtKey(oneFromListAction) != 0
+*/
+
 
 class TestButtonEditDialog: public QObject
 {
@@ -24,4 +52,4 @@ TestButtonEditDialog::TestButtonEditDialog(QObject* parent) :
 }
 
 // QTEST_MAIN(TestButtonEditDialog)
-#include "testbuttoneditdialog.cpp"
+#include "testbuttoneditdialog.moc"
