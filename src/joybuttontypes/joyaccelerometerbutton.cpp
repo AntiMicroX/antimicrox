41120ef45e827540a1005adc5d6a2338b66f4c27
Max Maisel <max.maisel@posteo.de>
2022-05-05 17:22:53 +0200

    Add JoySensorButton class
    A specialized JoyButton subclass is necessary to store a pointer to the
    JoySensor, to perform specialized mouse calculations and to store the
    new 3D direction.
    Add two additional subclasses for acceleromters and gyroscopes which
    handle the translated direction name of the button.
    
    Encode the 3D directions into a bitfield. This makes it easier to check
    for combined three dimensional directions later. Furthermore, add
    dummies for the required JoySensor interfaces.

---
+++ src/joybuttontypes/joyaccelerometerbutton.cpp
@@ -0,0 +1,63 @@
+/* antimicrox Gamepad to KB+M event mapper
+ * Copyright (C) 2022 Max Maisel <max.maisel@posteo.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "joyaccelerometerbutton.h"
+#include "joysensordirection.h"
+
+JoyAccelerometerButton::JoyAccelerometerButton(JoySensor *sensor, int index, int originset, SetJoystick *parentSet,
+                                               QObject *parent)
+    : JoySensorButton(sensor, index, originset, parentSet, parent)
+{
+}
+
+/**
+ * @brief Get translated accelerometer direction name of this button
+ * @returns Translated direction name
+ */
+QString JoyAccelerometerButton::getDirectionName() const
+{
+    QString label = QString();
+
+    switch (m_index)
+    {
+    case JoySensorDirection::SENSOR_UP:
+        label.append(tr("Up"));
+        break;
+
+    case JoySensorDirection::SENSOR_DOWN:
+        label.append(tr("Down"));
+        break;
+
+    case JoySensorDirection::SENSOR_LEFT:
+        label.append(tr("Left"));
+        break;
+
+    case JoySensorDirection::SENSOR_RIGHT:
+        label.append(tr("Right"));
+        break;
+
+    case JoySensorDirection::SENSOR_BWD:
+        label.append(tr("Shock"));
+        break;
+
+    default:
+        WARN() << "unknown accelerometer direction";
+        break;
+    }
+
+    return label;
+}
