0693758c769a69bf0a403a76617b97790cf3f509
Paweł Kotiuk <kotiuk@zohomail.eu>
2022-10-23 18:56:57 +0200

    refactor: Remove unnecessary usage of pointers in XMLConfigReader

--- src/xmlconfigreader.h
+++ src/xmlconfigreader.h
@@ -20,7 +20,6 @@
 #define XMLCONFIGREADER_H
 
 #include <QObject>
-#include <QPointer>
 #include <QStringList>
 
 class InputDevice;
@@ -48,7 +47,6 @@ class XMLConfigReader : public QObject
     QString const &getFileName();
     const QFile *getConfigFile();
     const InputDevice *getJoystick();
-    QPointer<InputDeviceXml> m_joystickXml;
     QStringList const &getDeviceTypes();
 
   protected:
