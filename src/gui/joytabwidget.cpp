531d377376fc82b60ebd0e3fa305ddfc2c10783f
Paweł Kotiuk <pawel@kotiuk.pl>
2025-12-26 15:57:02 +0100

    fix formatting

--- src/gui/joytabwidget.cpp
+++ src/gui/joytabwidget.cpp
@@ -1318,45 +1318,21 @@ void JoyTabWidget::changeCurrentSet(int index)
     }
 }
 
-void JoyTabWidget::changeSetOne()
-{
-    m_joystick->setActiveSetNumber(0);
-}
+void JoyTabWidget::changeSetOne() { m_joystick->setActiveSetNumber(0); }
 
-void JoyTabWidget::changeSetTwo()
-{
-    m_joystick->setActiveSetNumber(1);
-}
+void JoyTabWidget::changeSetTwo() { m_joystick->setActiveSetNumber(1); }
 
-void JoyTabWidget::changeSetThree()
-{
-    m_joystick->setActiveSetNumber(2);
-}
+void JoyTabWidget::changeSetThree() { m_joystick->setActiveSetNumber(2); }
 
-void JoyTabWidget::changeSetFour()
-{
-    m_joystick->setActiveSetNumber(3);
-}
+void JoyTabWidget::changeSetFour() { m_joystick->setActiveSetNumber(3); }
 
-void JoyTabWidget::changeSetFive()
-{
-    m_joystick->setActiveSetNumber(4);
-}
+void JoyTabWidget::changeSetFive() { m_joystick->setActiveSetNumber(4); }
 
-void JoyTabWidget::changeSetSix()
-{
-    m_joystick->setActiveSetNumber(5);
-}
+void JoyTabWidget::changeSetSix() { m_joystick->setActiveSetNumber(5); }
 
-void JoyTabWidget::changeSetSeven()
-{
-    m_joystick->setActiveSetNumber(6);
-}
+void JoyTabWidget::changeSetSeven() { m_joystick->setActiveSetNumber(6); }
 
-void JoyTabWidget::changeSetEight()
-{
-    m_joystick->setActiveSetNumber(7);
-}
+void JoyTabWidget::changeSetEight() { m_joystick->setActiveSetNumber(7); }
 
 void JoyTabWidget::showStickAssignmentDialog()
 {
